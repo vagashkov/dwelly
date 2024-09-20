fd59b297d79d048a3ca2e6417339522e089bb636
vagashkov <vitali.agashkov@gmail.com>
2024-09-20 14:04:22 +0300

    Initial project setup

---
+++ src/config/settings.py
@@ -0,0 +1,121 @@
+"""
+Django settings for dwelly project.
+
+"""
+from environs import Env
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / "subdir".
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Load sensitive settings from .env file
+env = Env()
+env.read_env(BASE_DIR / ".env")
+
+SECRET_KEY: str = env.str("SECRET_KEY")
+DEBUG: bool = env.bool("DEBUG", default=False)
+
+# DOMAINS section
+SECURE_MODE: bool = env.bool("SECURE_MODE", True)
+HTTP_SCHEME: str = "https" if SECURE_MODE else "http"
+
+DOMAIN_NAME: str = "127.0.0.1"
+PORT_NUMBER: int = 8000
+
+DOMAIN_ADDRESS: str = DOMAIN_NAME
+if PORT_NUMBER != 80:
+    DOMAIN_ADDRESS = "{}:{}".format(
+        DOMAIN_ADDRESS,
+        PORT_NUMBER
+    )
+
+ALLOWED_HOSTS = [
+    DOMAIN_NAME,
+]
+
+# Applications definition
+DJANGO_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
+
+THIRD_PARTY_APPS = [
+
+]
+
+PROJECT_APPS = [
+
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + PROJECT_APPS
+
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "config.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "config.wsgi.application"
+
+# Database
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+# Password validation
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",  # noqa: E501
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",  # noqa: E501
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",  # noqa: E501
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",  # noqa: E501
+    },
+]
+
+# Internationalization
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
+USE_I18N = True
+USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = "static/"
+
+# Default primary key field type
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
