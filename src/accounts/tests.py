219bae8e58a2861d49a10c01c208a4f5d0f0c7a8
vagashkov <vitali.agashkov@gmail.com>
2024-10-02 20:51:31 +0300

    Add accounts app initial tests (signup, login, logout)

--- src/accounts/tests.py
+++ src/accounts/tests.py
@@ -1,33 +1,108 @@
-from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-CustomUser = get_user_model()
+from .models import Account
+
+email = "newuser@email.com"
+password = "S0meStr0ngPaSSw0rd"
 
 
-class SignUpPageTests(TestCase):
+class SignUpPage(TestCase):
     """
     Test user signup process
     """
 
-    username = "newuser"
-    email = "newuser@email.com"
+    def test_signup_by_email(self):
+        """
+        Test if user can be created using email and password only
+        :return:
+        """
+        Account.objects.create_user(
+            email=email,
+            password=password
+        )
+        self.assertEqual(Account.objects.all().count(), 1)
+        self.assertEqual(
+            Account.objects.all()[0].email,
+            email
+        )
 
-    def setUp(self):
-        url = reverse("account_signup")
-        self.response = self.client.get(url)
-
-    def test_signup_template(self):
+    def test_signup_page(self):
+        """
+        Test user signup form existence
+        :return:
+        """
+        self.response = self.client.get(
+            reverse("account_signup")
+        )
         self.assertEqual(self.response.status_code, 200)
         self.assertTemplateUsed(self.response, "account/signup.html")
         self.assertContains(self.response, "Sign Up")
         self.assertNotContains(self.response, "Some wrong text")
 
-    def test_signup_form(self):
-        get_user_model().objects.create_user(self.username, self.email)
-        self.assertEqual(get_user_model().objects.all().count(), 1)
-        self.assertEqual(
-            get_user_model().objects.all()[0].username,
-            self.username
+
+class LoginPage(TestCase):
+    """
+    Test user login process
+    """
+
+    def setUp(self):
+        Account.objects.create_user(
+            email=email,
+            password=password
+            )
+
+    def test_login_page(self):
+        """
+        Test login page existence
+        :return:
+        """
+
+        self.response = self.client.get(
+            reverse("account_login")
         )
-        self.assertEqual(get_user_model().objects.all()[0].email, self.email)
+
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, "account/login.html")
+        self.assertContains(self.response, "Login")
+        self.assertNotContains(self.response, "Some wrong text")
+
+    def test_login_process(self):
+        """
+        Test user login process
+        :return:
+        """
+
+        self.response = self.client.post(
+            reverse("account_login"),
+            {
+                "login": email,
+                "password": password
+            }
+        )
+
+        # after successful authorization user
+        # should be redirected to the home page
+        self.assertEqual(self.response.status_code, 302)
+        self.assertEqual(
+            self.response.headers.get("Location"),
+            reverse("home")
+        )
+
+    def test_logout_process(self):
+        """
+        Test user logout process
+        :return:
+        """
+
+        self.response = self.client.get(
+            reverse("account_logout")
+        )
+
+        # after successful logout  user
+        # should be redirected to the home page
+        self.assertEqual(self.response.status_code, 302)
+        self.assertEqual(
+            self.response.headers.get("Location"),
+            reverse("home")
+        )
