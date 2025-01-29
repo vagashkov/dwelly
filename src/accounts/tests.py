4e7599a422c81065911f3bba6d8f83a449bc17f9
vagashkov <vitali.agashkov@gmail.com>
2025-01-29 14:18:54 +0300

    Added tests for signup/login/logout cases via web

--- src/accounts/tests.py
+++ src/accounts/tests.py
@@ -1,6 +1,6 @@
-from django.test import TestCase
-
 from django.db.utils import IntegrityError
+from django.test import TestCase
+from django.urls import reverse
 
 from .models import Account
 
@@ -103,3 +103,89 @@ class AccountTest(TestCase):
         self.assertTrue(account.is_active)
         self.assertTrue(account.is_staff)
         self.assertTrue(account.is_superuser)
+
+
+class SignUpPage(TestCase):
+    """
+    Test user signup process
+    """
+
+    def test_signup_page(self):
+        """
+        Test user signup form existence
+        :return:
+        """
+        self.response = self.client.get(
+            reverse("account_signup")
+        )
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, "account/signup.html")
+        self.assertContains(self.response, "Sign Up")
+        self.assertNotContains(self.response, "Some wrong text")
+
+
+class LoginPage(TestCase):
+    """
+    Test user login process
+    """
+
+    def setUp(self):
+        Account.objects.create_user(
+            email=email,
+            password=password
+            )
+
+    def test_login_page(self):
+        """
+        Test login page existence
+        :return:
+        """
+
+        self.response = self.client.get(
+            reverse("account_login")
+        )
+
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, "account/login.html")
+        self.assertContains(self.response, "Sign In")
+        self.assertNotContains(self.response, "Some wrong text")
+
+    def test_login_process(self):
+        """
+        Test user login process
+        :return:
+        """
+
+        self.response = self.client.post(
+            reverse("account_login"),
+            {
+                "login": email,
+                "password": password
+            }
+        )
+
+        # after successful authorization user
+        # should be redirected to the home page
+        self.assertEqual(self.response.status_code, 302)
+        self.assertEqual(
+            self.response.headers.get("Location"),
+            reverse("home")
+        )
+
+    def test_logout_process(self):
+        """
+        Test user logout process
+        :return:
+        """
+
+        self.response = self.client.get(
+            reverse("account_logout")
+        )
+
+        # after successful logout  user
+        # should be redirected to the home page
+        self.assertEqual(self.response.status_code, 302)
+        self.assertEqual(
+            self.response.headers.get("Location"),
+            reverse("home")
+        )
