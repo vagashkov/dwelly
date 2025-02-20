3a2a39419c70b7fa6d6e6f309912670345f50d96
vagashkov <vitali.agashkov@gmail.com>
2025-02-20 16:16:25 +0300

    Switch to PostgreSQL (fix)

--- src/config/settings.py
+++ src/config/settings.py
@@ -110,13 +110,6 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 WSGI_APPLICATION = "config.wsgi.application"
 
 # Database
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-# Database credentials
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
