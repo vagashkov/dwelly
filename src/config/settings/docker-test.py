dceef5ff3b390a98716a4c37ce8210e9b7588301
vagashkov <vitali.agashkov@gmail.com>
2025-08-04 14:30:22 +0300

    Add docker-based deployment tests

---
+++ src/config/settings/docker-test.py
@@ -0,0 +1,38 @@
+from ff3 import FF3Cipher
+
+from .base import *  # noqa: F401, F403
+
+DEBUG = True
+ALLOWED_HOSTS = []
+SECRET_KEY = "django-insecure-hz^x4#xvj7p@6p2h64ssh9*uccr-7ecjcmi2944!pf!sk6&e%@"  # noqa: E501
+
+# Database connection credentials
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": 5432
+    }
+}
+
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
+
+# FF3 encrypter settings (read https://pypi.org/project/ff3/ for details)
+FF3_KEY = "C4A5CEFE80FA957333EA7947AC284467"
+FF3_TWEAK = "01D250AAD1B8B6"
+FF3_LENGTH = 6
+FF3_CIPHER = FF3Cipher(FF3_KEY, FF3_TWEAK)
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static"  # noqa: F405
+]
+
+# Media files storage configuration
+MEDIA_ROOT = BASE_DIR.joinpath("media")  # noqa: F405
+MEDIA_URL = "/media/"
